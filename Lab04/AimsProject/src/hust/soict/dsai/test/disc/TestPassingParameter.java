7e1cf554955d2ab555314abe9db471fd76323329
ptkiennn0901 <kien.pt0901@gmail.com>
2024-11-29 17:56:42 +0700

    topic/aims-project/console

---
+++ Lab04/AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
@@ -0,0 +1,36 @@
+package hust.soict.dsai.test.disc;
+
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+
+public class TestPassingParameter {
+	public static void main(String[] args) {
+		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+		
+		new_swap(jungleDVD, cinderellaDVD);
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+		System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+		
+		changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+	}
+
+	public static void swap(Object o1, Object o2) {
+		Object tmp = o1;
+		o1 = o2;
+		o2 = tmp;
+	}
+	
+	public static void new_swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+		DigitalVideoDisc temp = new DigitalVideoDisc(dvd2.getTitle());
+		dvd2.setTitle(dvd1.getTitle());
+		dvd1.setTitle(temp.getTitle());
+	}
+	
+	public static void changeTitle(DigitalVideoDisc dvd, String title) {
+		String oldTitle = dvd.getTitle();
+		dvd.setTitle(title);
+		dvd = new DigitalVideoDisc(oldTitle);
+	}
+	
+}
